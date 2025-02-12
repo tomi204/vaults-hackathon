c01f036a7523e53e6b0c1fad9755f6a4de50a0c7
Tomas Oliver <tomi204@outlook.com>
2025-02-12 12:39:10 -0300

    fix: clean repo

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -2,23 +2,12 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
+import {SuperVault} from "../src/SuperVault.sol";
 
-contract CounterTest is Test {
-    Counter public counter;
+contract SuperVaultTest is Test {
+    SuperVault public superVault;
 
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
-    }
-
-    function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
-    }
-
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+        superVault = new SuperVault();
     }
 }
